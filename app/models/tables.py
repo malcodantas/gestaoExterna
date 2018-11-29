9bbb5d91d248a08a6f5e9a2472d96b7677373aa0
Malco Dantas <malcodantas@live.com>
2018-11-29 02:50:58 -0300

    Nova tabela de equipamentos

--- app/models/tables.py
+++ app/models/tables.py
@@ -12,4 +12,17 @@ class User(db.Model,UserMixin):
         self.password=password
     def __repr__(self):
         return self.username
-#class Equipamento(db.Model):
+
+class Equipamento(db.Model):
+    __tablename__="equipamentos"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+    status=db.Column(db.Boolean)
+    descricao=db.Column(db.String(200))
+    def __init__(self,name,status,descricao):
+        self.name=name
+        self.status=status
+        self.descricao=descricao
+    def __repr__(self):
+        return self.name
+
